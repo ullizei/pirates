1ac0327a010b09d986c956bb27f90d099ed6d766
ullizei <ulrika.hjalmgarden@gmail.com>
2014-05-30 09:36:09 +0200

    started working opn crew inspector

---
+++ Pirates/Assets/Scripts/GUI/StatsPanel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsPanel : MonoBehaviour {
+
+	public TextMesh inspectedCharacterName;
+	public TextMesh inspectedCharacterJobLevel;
+
+	private CharacterData inspectedCharacterData;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void ShowStats(CharacterData selecetdCharacter)
+	{
+		inspectedCharacterData = selecetdCharacter;
+		inspectedCharacterName.text = selecetdCharacter.name;
+		inspectedCharacterJobLevel.text = "Level "+selecetdCharacter.level+" "+selecetdCharacter.job;
+	}
+}
